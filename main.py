65f95fd891d023c422f4f4148122ed76d02bbe58
mateusz <259160@student.pwr.edu.pl>
2022-10-16 22:26:55 +0200

    final
    updated programm

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 
-
 def startup():
-    update_viewport(None, 400, 400)
+    update_viewport(None, 800, 800)
     glClearColor(0.5, 0.5, 0.5, 1.0)
 
 
@@ -16,18 +15,36 @@ def shutdown():
     pass
 
 
-def recurrence(size_min, size_max, maxstep):
-
-    glColor3ub(0, 126, 0)
-    glBegin(GL_TRIANGLE_STRIP)
-    glVertex2f(size_min*1/(maxstep*3), size_min*1/(maxstep*3))
-    glVertex2f(size_max*1/(maxstep*3), size_min*1/(maxstep*3))
-    glVertex2f(size_min*1/(maxstep*3), size_max*1/(maxstep*3))
-    glVertex2f(size_max*1/(maxstep*3), size_max*1/(maxstep*3))
-    glEnd()
+def check(num):
+    z = 0.0
+    n = 0
+    while abs(z) <= 2 and n < 50:
+        z = z*z + num
+        n += 1
+    return n
 
 
-def rect(x, y, size):
+def recursion(x, y, size, steps):   #draws 8 rectangles arround a given one
+        if steps == 0: return
+        rect(x - size * 2 / 3, y + size * 2 / 3, size / 3)
+        rect(x + size * 1 / 3, y + size * 2 / 3, size / 3)
+        rect(x + size * 4 / 3, y + size * 2 / 3, size / 3)
+        rect(x - size * 2 / 3, y - size * 1 / 3, size / 3)
+        rect(x + size * 4 / 3, y - size * 1 / 3, size / 3)
+        rect(x - size * 2 / 3, y - size * 4 / 3, size / 3)
+        rect(x + size * 1 / 3, y - size * 4 / 3, size / 3)
+        rect(x + size * 4 / 3, y - size * 4 / 3, size / 3)
+        recursion(x - size * 2 / 3, y + size * 2 / 3, size / 3, steps - 1)
+        recursion(x + size * 1 / 3, y + size * 2 / 3, size / 3, steps - 1)
+        recursion(x + size * 4 / 3, y + size * 2 / 3, size / 3, steps - 1)
+        recursion(x - size * 2 / 3, y - size * 1 / 3, size / 3, steps - 1)
+        recursion(x + size * 4 / 3, y - size * 1 / 3, size / 3, steps - 1)
+        recursion(x - size * 2 / 3, y - size * 4 / 3, size / 3, steps - 1)
+        recursion(x + size * 1 / 3, y - size * 4 / 3, size / 3, steps - 1)
+        recursion(x + size * 4 / 3, y - size * 4 / 3, size / 3, steps - 1)
+
+
+def rect(x, y, size): #draws a rectangle given top left corner and size
     glColor3ub(230, 230, 230)
     glBegin(GL_TRIANGLE_STRIP)
     glVertex2f(x,y)
@@ -37,23 +54,38 @@ def rect(x, y, size):
     glEnd()
 
 
-def render(time):
-    size_min = -100                         #size of the frame
-    size_max = 100
-    glClear(GL_COLOR_BUFFER_BIT)
-    glColor3ub(144, 128, 0)                 #color of the background
-
-    glBegin(GL_TRIANGLE_STRIP)              #generating background
-    glVertex2f(size_min, size_min)
-    glVertex2f(size_max, size_min)
-    glVertex2f(size_min, size_max)
-    glVertex2f(size_max,size_max)
-    glEnd()
-    rect(-100/3, 100/3,100*2/3)             #drawing rectangle using one corner and length
+def render(time,no_steps,program):
+    if program == 1:
+        size_min = -800                         #size of the frame
+        size_max = 800
+        glClear(GL_COLOR_BUFFER_BIT)
+        glColor3ub(144, 128, 0)                 #color of the background
     
-
-    glFlush()
-
+        glBegin(GL_TRIANGLE_STRIP)              #generating background
+        glVertex2f(size_min, size_min)
+        glVertex2f(size_max, size_min)
+        glVertex2f(size_min, size_max)
+        glVertex2f(size_max,size_max)
+        glEnd()
+        rect(-800/3, 800/3,800*2/3)             #first rectangle
+        recursion(-800 / 3, 800 / 3, 800 * 2 / 3, no_steps)
+        glFlush()
+    if program == 2:
+        real_start = -2
+        real_end = 1
+        im_start = -1
+        im_end = 1
+        _range = 800
+        for row in range(_range):
+            for column in range(_range):
+                c = complex(real_start + (row / 800) * (real_end - real_start),
+                            im_start + (column / 800) * (im_end - im_start))
+                data = check(c)
+                glBegin(GL_POINTS)
+                glColor3ub(255 - int(data * 255 / 50), 400 - int(data * 400 / 50), 0)
+                glVertex2f(2 * row - 800, 2 * column - 800)
+                glEnd()
+        glFlush()
 
 
 def update_viewport(window, width, height):
@@ -68,10 +100,10 @@ def update_viewport(window, width, height):
     glLoadIdentity()
 
     if width <= height:
-        glOrtho(-100.0, 100.0, -100.0 / aspect_ratio, 100.0 / aspect_ratio,
+        glOrtho(-800.0, 800.0, -800.0 / aspect_ratio, 800.0 / aspect_ratio,
                 1.0, -1.0)
     else:
-        glOrtho(-100.0 * aspect_ratio, 100.0 * aspect_ratio, -100.0, 100.0,
+        glOrtho(-800.0 * aspect_ratio, 800.0 * aspect_ratio, -800.0, 800.0,
                 1.0, -1.0)
 
     glMatrixMode(GL_MODELVIEW)
@@ -79,10 +111,17 @@ def update_viewport(window, width, height):
 
 
 def main():
+    print("Which program do you want to open:\n1. Sierpinski Carpet\n2. Mandelbrot Set")
+    program = int(input())
+    steps=0
+    if program==1:
+        print("How many steps of recursion do you want? \nMore than 7 may be too much ;) \nNumber of steps:")
+        steps = int(input())
+
     if not glfwInit():
         sys.exit(-1)
 
-    window = glfwCreateWindow(400, 400, __file__, None, None)
+    window = glfwCreateWindow(800, 800, __file__, None, None)
     if not window:
         glfwTerminate()
         sys.exit(-1)
@@ -93,7 +132,8 @@ def main():
 
     startup()
     while not glfwWindowShouldClose(window):
-        render(glfwGetTime())
+
+        render(glfwGetTime(),steps,program)
         glfwSwapBuffers(window)
         glfwPollEvents()
     shutdown()
