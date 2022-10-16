027750eea51c4a20ded92bb35392515db96148bf
mateusz <259160@student.pwr.edu.pl>
2022-10-16 16:54:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+import sys
+
+from glfw.GLFW import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+
+
+def startup():
+    update_viewport(None, 400, 400)
+    glClearColor(0.5, 0.5, 0.5, 1.0)
+
+
+def shutdown():
+    pass
+
+
+def recurrence(size_min, size_max, maxstep):
+
+    glColor3ub(0, 126, 0)
+    glBegin(GL_TRIANGLE_STRIP)
+    glVertex2f(size_min*1/(maxstep*3), size_min*1/(maxstep*3))
+    glVertex2f(size_max*1/(maxstep*3), size_min*1/(maxstep*3))
+    glVertex2f(size_min*1/(maxstep*3), size_max*1/(maxstep*3))
+    glVertex2f(size_max*1/(maxstep*3), size_max*1/(maxstep*3))
+    glEnd()
+
+
+def rect(x, y, size):
+    glColor3ub(230, 230, 230)
+    glBegin(GL_TRIANGLE_STRIP)
+    glVertex2f(x,y)
+    glVertex2f(x+size,y)
+    glVertex2f(x,y-size)
+    glVertex2f(x+size,y-size)
+    glEnd()
+
+
+def render(time):
+    size_min = -100                         #size of the frame
+    size_max = 100
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3ub(144, 128, 0)                 #color of the background
+
+    glBegin(GL_TRIANGLE_STRIP)              #generating background
+    glVertex2f(size_min, size_min)
+    glVertex2f(size_max, size_min)
+    glVertex2f(size_min, size_max)
+    glVertex2f(size_max,size_max)
+    glEnd()
+    rect(-100/3, 100/3,100*2/3)             #drawing rectangle using one corner and length
+    
+
+    glFlush()
+
+
+
+def update_viewport(window, width, height):
+    if width == 0:
+        width = 1
+    if height == 0:
+        height = 1
+    aspect_ratio = width / height
+
+    glMatrixMode(GL_PROJECTION)
+    glViewport(0, 0, width, height)
+    glLoadIdentity()
+
+    if width <= height:
+        glOrtho(-100.0, 100.0, -100.0 / aspect_ratio, 100.0 / aspect_ratio,
+                1.0, -1.0)
+    else:
+        glOrtho(-100.0 * aspect_ratio, 100.0 * aspect_ratio, -100.0, 100.0,
+                1.0, -1.0)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
+def main():
+    if not glfwInit():
+        sys.exit(-1)
+
+    window = glfwCreateWindow(400, 400, __file__, None, None)
+    if not window:
+        glfwTerminate()
+        sys.exit(-1)
+
+    glfwMakeContextCurrent(window)
+    glfwSetFramebufferSizeCallback(window, update_viewport)
+    glfwSwapInterval(1)
+
+    startup()
+    while not glfwWindowShouldClose(window):
+        render(glfwGetTime())
+        glfwSwapBuffers(window)
+        glfwPollEvents()
+    shutdown()
+
+    glfwTerminate()
+
+if __name__ == '__main__':
+    main()
+
